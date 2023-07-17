3397b09cb215816161e9885b2390b4479f64277e
ktk8916 <ktk8916@naver.com>
2023-07-17 14:06:29 +0900

    email 컬럼 유니크 조건 추가

--- src/main/java/com/playdata/todoapp/member/domain/entitiy/Member.java
+++ src/main/java/com/playdata/todoapp/member/domain/entitiy/Member.java
@@ -15,6 +15,7 @@ public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String email;
     private String name;
     private String password;
