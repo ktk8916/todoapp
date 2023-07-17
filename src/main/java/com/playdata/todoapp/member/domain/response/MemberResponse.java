1ab238826183611acf69772f347d2d4438dfe1c9
ktk8916 <ktk8916@naver.com>
2023-07-17 16:58:50 +0900

    멤버찾기 응답 양식 변경

--- src/main/java/com/playdata/todoapp/member/domain/response/MemberResponse.java
+++ src/main/java/com/playdata/todoapp/member/domain/response/MemberResponse.java
@@ -1,18 +1,17 @@
 package com.playdata.todoapp.member.domain.response;
 
+import com.playdata.todoapp.member.domain.dto.MemberDto;
 import com.playdata.todoapp.member.domain.entitiy.Member;
 import com.playdata.todoapp.todo.domain.dto.TodoDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record MemberResponse(Long id, String name, Integer age, List<TodoDto> todos) {
+public record MemberResponse(MemberDto member, List<TodoDto> todos) {
 
     public static MemberResponse from(Member member){
         return new MemberResponse(
-                member.getId(),
-                member.getName(),
-                member.getAge(),
+                MemberDto.from(member),
                 member.getTodos()
                         .stream()
                         .map(TodoDto::from)
