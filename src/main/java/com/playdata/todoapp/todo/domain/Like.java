ea07c29fb8e6a7201c896f3bb58695b40b7b19ff
ktk8916 <ktk8916@naver.com>
2023-07-17 10:27:26 +0900

    like entity 추가

--- src/main/java/com/playdata/todoapp/todo/domain/Like.java
+++ src/main/java/com/playdata/todoapp/todo/domain/Like.java
@@ -4,23 +4,18 @@ import com.playdata.todoapp.member.domain.entitiy.Member;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
-
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Builder
-public class Todo {
+public class Like {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
-    private Boolean isDone;
-    private Integer likeCount;
     @ManyToOne
     private Member member;
-    @OneToMany(mappedBy = "todo")
-    private List<Like> likes;
+    @ManyToOne
+    private Todo todo;
 
 }
